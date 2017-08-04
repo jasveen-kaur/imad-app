6e4937454edb7beb4e9c4fb50411b99c3fe66b69
jasveen-kaur <30661580+jasveen-kaur@users.noreply.github.com>
2017-08-04 20:04:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ element.innerHTML="Javeen Kaur";
 //move the img
 var img =document.getElementById("modi");
 
-img.onClick= function(){
+img.onclick= function(){
     img.style.marginRight="100px";
 };
