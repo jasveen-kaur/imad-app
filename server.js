9dd9c7461ec9b2c1249814c572e21f818dedc274
jasveen-kaur <30661580+jasveen-kaur@users.noreply.github.com>
2017-08-25 02:29:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/login', function(req,res){
     var username =req.body.username; 
    var password =req.body.password; 
    
-   pool.query('select  * from "user" where username= $1', [username], function(err, result){
+   pool.query('select  * from "user" where username= $1' [username], function(err, result){
        if(err){
            res.status(500).send(err.toString());
        } else{
