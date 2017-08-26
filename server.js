a5cbaad7a4616d9a93ca15ccb50713b9950bd511
jasveen-kaur <30661580+jasveen-kaur@users.noreply.github.com>
2017-08-26 23:47:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,6 +123,11 @@ app.get('/check-login' , function(req,res){
     }
 });
 
+app.get('/logout' , function(req,res){
+    delete req.session.auth;
+        res.send('Logged out');
+});
+
 var pool= new Pool(config);
 app.get('/test-db', function (req,res){
     pool.query('SELECT * FROM test', function (err,result){
